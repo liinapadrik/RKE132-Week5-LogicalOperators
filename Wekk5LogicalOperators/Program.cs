2286c9645ca3d3e8181ae6a5924e47a7c4562a5c
liinapadrik <liina.padrik@tktk.ee>
2024-09-10 10:22:29 +0300

    add week5 code

---
+++ Wekk5LogicalOperators/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+//AND - &&
+
+//true && true --> true
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+
+//OR - II (pipes)
+
+//true II true --> true
+//true II false --> true
+//false II true --> true
+// false II false --> false
